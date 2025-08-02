efa315e54a62f51d898ef0837dee8d71ecf9b1e6
mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2025-08-02 21:46:37 +0300

    add_component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import { ScientistsSection } from './components/ScientistsSection'
 import { FieldsSection } from './components/FieldsSection'
 import { TimelineSection } from './components/TimelineSection'
 import { Footer } from './components/Footer'
+import { useParams } from "react-router-dom";
+import { ScientistDetail } from './components/ScientistDetail';
+
+export function ScientistDetail() {
+  const { id } = useParams();
+  // You can fetch or import your scientists data here
+  return (
+    <div className="container py-10">
+      <h1>صفحة العالم رقم {id}</h1>
+      {/* تفاصيل العالم هنا */}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
