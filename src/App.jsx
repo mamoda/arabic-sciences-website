ce4f03bbdefdfb488dfefa3ec87fc1b59e38f0cc
mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2025-08-02 21:41:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
 import { Header } from './components/Header'
 import { HeroSection } from './components/HeroSection'
 import { ScientistsSection } from './components/ScientistsSection'
 import { FieldsSection } from './components/FieldsSection'
 import { TimelineSection } from './components/TimelineSection'
 import { Footer } from './components/Footer'
-<Route path="/scientist/:id" element={<ScientistDetail scientists={scientists} />} />
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Header />
       <main>
-        <HeroSection />
-        <ScientistsSection />
-        <FieldsSection />
-        <TimelineSection />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <ScientistsSection />
+              <FieldsSection />
+              <TimelineSection />
+            </>
+          } />
+          {<Route path="/scientist/:id" element={<ScientistDetail scientists={scientists} />} />
+}
+        </Routes>
       </main>
       <Footer />
     </div>
