ee1e87666a56f0b0599f32936c31fa9a890778b5
mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2025-08-12 21:54:37 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { ScientistDetail } from './components/ScientistDetail';
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { Profile } from "./pages/Profile";
+import { BooksPage } from '/pages/BooksPage';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/books" element={<BooksPage />} />
         </Routes>
       </main>
       <Footer />
